e9ee7c86360d1f701e03899c284a5f4ffd663e87
Bexultan Myrzatay <bexultan.dev@gmail.com>
2019-01-15 23:28:56 +0600

    nav logo adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../young_developer_logo.png";
 
 export default () => (
   <nav className="navbar navbar-default navbar-expand-lg">
     <div className="container">
-      <Link to="/" className="navbar-brand" style={{minHeight: 70, paddingTop: 15, paddingBottom: 15}}>
-        <img
-          src="img/young_developer_logo.png"
-          width="147"
-          height="40"
-          alt="fff"
-        />
+      <Link
+        to="/"
+        className="navbar-brand"
+        style={{ minHeight: 70, paddingTop: 15, paddingBottom: 15 }}
+      >
+        <img src={logo} width="147" height="40" alt="fff" />
       </Link>
     </div>
   </nav>
