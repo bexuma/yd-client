2ea040e8f23ee79b3bc262a87e6f66125018be63
Bexultan Myrzatay <bexultan.dev@gmail.com>
2019-01-23 16:08:22 +0600

    Code syntax highlighting

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -6,6 +6,7 @@ import Spinner from "../Spinner";
 import { Link } from "react-router-dom";
 import "./stylePost.scss";
 // import materialKit from '../../material-kit';
+import CodeBlock from './CodeBlock'
 
 const POST_QUERY = gql`
   query Post($postSlug: String!) {
@@ -23,15 +24,13 @@ const POST_QUERY = gql`
 
 export class Post extends Component {
   renderPost = ({ slug, language, title, body }) => {
-    const avatarLinkedIn =
-      "https://media.licdn.com/dms/image/C5103AQH1gkmiVhd-lw/profile-displayphoto-shrink_200_200/0?e=1553126400&v=beta&t=1MoGtCyEFyDr8bPLpLEyPa30-RNz_Z_8cXtYNccNeO4";
     return (
       <div className="container" style={{ maxWidth: "1088px" }}>
         <div className="row">
           <div className="col-md-8" style={{ whiteSpace: "pre-line" }}>
             <h1 className="title">{title}</h1>
 
-            <ReactMarkdown className="post-body" source={body} />
+            <ReactMarkdown className="post-body" source={body} renderers={{code: CodeBlock}} />
             {!process.env.NODE_ENV || process.env.NODE_ENV === "development" ? (
               <Link to={`/posts/${slug}/edit`}>Edit</Link>
             ) : null}
