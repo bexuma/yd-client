38583f99eff4a1a2f1dc7cae8fc2ddc05850b4fd
Bexultan Myrzatay <bexultan.dev@gmail.com>
2019-01-30 22:49:30 +0600

    fix hor. overflow on pre tag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,19 +23,9 @@ export default () => {
         >
           <img src={logo} width="191" height="52" alt="fff" />
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+        
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse">
           <ul className="navbar-nav ml-auto">
             {Object.entries(icons).map((icon, index) => {
               return (
